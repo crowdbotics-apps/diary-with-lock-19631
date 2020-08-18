6488405c8aacdaba0c9b7a0fb29c185f27c97885
Crowdbotics <team@crowdbotics.com>
2020-08-18 01:46:44 +0000

    Initial commit.

---
+++ backend/diary_with_lock_19631/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for diary_with_lock_19631 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diary_with_lock_19631.settings')
+
+application = get_wsgi_application()
